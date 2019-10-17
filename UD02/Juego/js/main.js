6e92036e8f16d788d83aa17991e217d917fb5396
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-10-17 03:25:53 +0200

    Añadiendo cositas

--- UD02/Juego/js/main.js
+++ UD02/Juego/js/main.js
@@ -1,97 +1,111 @@
-
 // comprobar bloques con includes
 // mapa[i][j].value.classList.value.includes();
+/* if (mapa[i][j].value.classList.value.includes(personaje)) {
+        newDiv.classList.replace("personaje", "huellas");
+ }
+        document.querySelector(".personaje").classList.replace("personaje", "huellas");  
+
+
+         var query = document.querySelector(".personaje");
+    query.classList.add("huellas");
+    query.classList.remove;
+   */
+
 
 var bart = {};
-bart.X=0;
-bart.Y=8;
-
-mapa =[
-    [9,9,9,9,9,9,9,9,2,9,9,9,9,9,9,9,9,9,9,9,9],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-    [0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-    [0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-    [0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-    [0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0],
-    [0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]];
+bart.X = 0;
+bart.Y = 8;
 
 
-    
-window.onload=function(){
- 
+
+mapa = [
+    [9, 9, 9, 9, 9, 9, 9, 9, 2, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+    [0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+
+
+window.onload = function() {
+
     listo()
 };
 
-function listo(){
+function listo() {
 
-dibujarMapa();
-document.addEventListener("keydown", cogerTecla);
+    dibujarMapa();
+    document.addEventListener("keydown", cogerTecla);
 
 }
 
 
-function dibujarMapa(){
+function dibujarMapa() {
 
-    document.querySelector(".mapa").innerHTML="";
+    document.querySelector(".mapa").innerHTML = "";
 
-    for (var i=0;i<14;i++){
-        for (var j=0;j<21;j++){
+    for (var i = 0; i < 14; i++) {
+        for (var j = 0; j < 21; j++) {
             var newDiv = document.createElement("div");
-            if (mapa[i][j]==0){
+            if (mapa[i][j] == 0) {
                 newDiv.classList.add("camino");
-            }else if (mapa[i][j]==1) {
+            } else if (mapa[i][j] == 1) {
                 newDiv.classList.add("bloques");
-            }
-            else if (mapa[i][j]==2) {
-                
+            } else if (mapa[i][j] == 2) {
                 newDiv.classList.add("personaje");
-            }
-            else if (mapa[i][j]==3) {
-                
+            } else if (mapa[i][j] == 3) {
                 newDiv.classList.add("momia");
-            }
-            else if (mapa[i][j]==9) {
-                
+            } else if (mapa[i][j] == 4) {
+                newDiv.classList.add("huellas");
+            } else if (mapa[i][j] == 9) {
                 newDiv.classList.add("fondo");
             }
             document.querySelector(".mapa").appendChild(newDiv);
-          
+
+
         }
     }
-    
+
 }
 
 
-function cogerTecla(e){		
-    
+function cogerTecla(e) {
+
+
+
+
+
     var key_code = e.keyCode;
-    switch(key_code){
+    switch (key_code) {
         //flecha izquierda
-        case 37: 
+        case 37:
             moverIzquierda();
             break;
 
-        //flecha arriba
-        case 38: 
+            //flecha arriba
+        case 38:
             moverArriba();
             break;
 
-        //flecha derecha   
+            //flecha derecha   
         case 39:
             moverDerecha();
             break;
 
-        //flecha debajo    
-        case 40: 
-        moverDebajo();
-            break;						
+            //flecha debajo    
+        case 40:
+            moverDebajo();
+            break;
     }
 
 
@@ -99,59 +113,47 @@ function cogerTecla(e){
     dibujarMapa();
 }
 
-function moverPersonaje(){
-    mapa[bart.X][bart.Y]=2;
+function moverPersonaje() {
+    mapa[bart.X][bart.Y] = 2;
+
+
 }
 
+function moverIzquierda() {
 
-function moverIzquierda(){
-   
-     
-    if (bart.Y != 0 ) { 
+    if (bart.Y != 0) {
         console.log("Has pulsado flecha izquierda");
-        console.log("Y: " +bart.Y);
+        console.log("Y: " + bart.Y);
         bart.Y--;
-    } 
+    }
 }
-function moverArriba(){
-   
-   
-    if (bart.X != 1) {
 
+function moverArriba() {
+
+    if (bart.X != 1) {
         console.log("Has pulsado flecha arriba");
-        console.log("X: " + bart.X);    
+        console.log("X: " + bart.X);
         bart.X--;
     }
 }
-function moverDerecha(){
-    
+
+function moverDerecha() {
+
     if (bart.Y != 20) {
         console.log("Has pulsado flecha derecha");
-    console.log("Y: " + bart.Y);
+        console.log("Y: " + bart.Y);
         bart.Y++;
     }
 }
-function moverDebajo(){
-  
+
+function moverDebajo() {
+
+
     if (bart.X != 13) {
         console.log("Has pulsado flecha debajo");
         console.log("X: " + bart.X);
-
-        
         bart.X++;
 
     }
-  
+
 }
-
-
-
-
-
-
-
-
-
- 
-
-
