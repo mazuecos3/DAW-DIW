38042e867e24d5667592d18ca0e18f1c225f95bf
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-10-23 00:42:06 +0200

    +Comprobar bloques casi terminado Falta solucionar algun pequeño bug mañana avanzar con moverSkinner

--- UD02/Juego/js/main.js
+++ UD02/Juego/js/main.js
@@ -35,7 +35,7 @@ window.onload = function() {
 function listo() {
     dibujarMapa();
     document.addEventListener("keydown", cogerTecla);
-   setInterval(moverSkinner, 300); 
+    setInterval(moverSkinner, 1000);
 }
 
 function dibujarMapa() {
@@ -85,7 +85,7 @@ function dibujarMapa() {
             document.querySelector(".mapa").appendChild(newDiv);
         }
     }
-    
+
 }
 
 function cogerTecla(e) {
@@ -156,7 +156,7 @@ function moverIzquierda() {
 
     }
     comprobarBloques();
- 
+
 }
 
 function moverDerecha() {
@@ -267,28 +267,28 @@ function moverDebajo() {
 //MOVER SKINNER
 
 function moverSkinner() {
-    
-  let movimientoRandom =  Math.floor(Math.random() * 4);
+
+    let movimientoRandom = Math.floor(Math.random() * 4);
     switch (movimientoRandom) {
 
-            case 0:  
-                moverIzquierdaSkinner();
+        case 0:
+            moverIzquierdaSkinner();
             break;
 
-            case 1:
-                moverDerechaSkinner();
+        case 1:
+            moverDerechaSkinner();
             break;
 
-            case 2:
-                moverArribaSkinner();
+        case 2:
+            moverArribaSkinner();
             break;
 
-            case 3:
-                moverDebajoSkinner();
+        case 3:
+            moverDebajoSkinner();
             break;
- 
+
     }
-    console.log(movimientoRandom);
+
 }
 
 
@@ -297,22 +297,21 @@ function moverSkinner() {
 function moverIzquierdaSkinner() {
     var aux = !mapa[skinner_Y][skinner_X - 1].classList.value.includes("camino");
     var aux1 = !mapa[skinner_Y][skinner_X - 1].classList.value.includes("huellas");
-    
+
     if (skinner_X != 0) {
 
         if (aux && aux1) {
-            console.log("Choque");
+
             //SINO CAMBIA PEROSNAJE POR HUELLA PARA DEJAR EL RASTRO DE HUELLAS
-        }else{
+        } else {
             mapa[skinner_Y][skinner_X].classList.replace("momia", "camino");
             skinner_X--;
             mapa[skinner_Y][skinner_X].classList.replace("camino", "momia");
-    } 
+        }
 
-}
-else{
-    moverDerechaSkinnerSkinner();
-}
+    } else {
+        moverDerechaSkinnerSkinner();
+    }
 }
 
 function moverDerechaSkinner() {
@@ -320,54 +319,56 @@ function moverDerechaSkinner() {
     var aux1 = !mapa[skinner_Y][skinner_X + 1].classList.value.includes("huellas");
 
     if (skinner_X != 21) {
-        
+
         if (aux && aux1) {
-            console.log("Choque ");
+
             //SINO CAMBIA PEROSNAJE POR HUELLA PARA DEJAR EL RASTRO DE HUELLAS
-        } else{
+        } else {
             mapa[skinner_Y][skinner_X].classList.replace("momia", "camino");
             skinner_X++;
             mapa[skinner_Y][skinner_X].classList.replace("camino", "momia");
-    } 
-}else{
-    moverArribaSkinner();
+        }
+    } else {
+        moverArribaSkinner();
+    }
+
 }
-   
-}
+
 function moverArribaSkinner() {
     var aux = !mapa[skinner_Y - 1][skinner_X].classList.value.includes("camino");
     var aux1 = !mapa[skinner_Y - 1][skinner_X].classList.value.includes("huellas");
 
     if (skinner_Y != 1) {
         if (aux && aux1) {
-            console.log("Choque ");
+
             //SINO CAMBIA PEROSNAJE POR HUELLA PARA DEJAR EL RASTRO DE HUELLAS
-        }else{
+        } else {
             mapa[skinner_Y][skinner_X].classList.replace("momia", "camino");
             skinner_Y--;
             mapa[skinner_Y][skinner_X].classList.replace("camino", "momia");
+        }
+    } else {
+        moverDebajoSkinner();
     }
-}else{
-    moverDebajoSkinner();
+
 }
-   
-}
+
 function moverDebajoSkinner() {
     var aux = !mapa[skinner_Y + 1][skinner_X].classList.value.includes("camino");
     var aux1 = !mapa[skinner_Y + 1][skinner_X].classList.value.includes("huellas");
 
     if (skinner_Y != 14) {
         if (aux && aux1) {
-            console.log("Choque");
+
             //SINO CAMBIA PEROSNAJE POR HUELLA PARA DEJAR EL RASTRO DE HUELLAS
-        }else{
+        } else {
             mapa[skinner_Y][skinner_X].classList.replace("momia", "camino");
             skinner_Y++;
             mapa[skinner_Y][skinner_X].classList.replace("camino", "momia");
+        }
+    } else {
+        moverIzquierdaSkinner();
     }
-}else{
-    moverIzquierdaSkinner();
-}
 }
 
 //BLOQUES RODEADOS
@@ -378,24 +379,46 @@ function comprobarBloques() {
 
     let dataFila;
     let dataColumna;
+    let arrayPremios = ["papiro", "llave", "sarcofago"];
 
     for (let i = 0; i < docBloques.length; i++) {
 
         //asignamos atributos filas y columnas
         dataFila = parseInt(docBloques[i].getAttribute("data-fila"));
         dataColumna = parseInt(docBloques[i].getAttribute("data-columna"));
-     
-        // si no contiene camino a la derecha,arriba,debajo o izquierda 
-         
-            if ( 
-                !mapa[dataFila - 1][dataColumna].classList.value.includes("camino") &&
-                !mapa[dataFila][dataColumna - 1].classList.value.includes("camino") &&
+
+        if (dataFila < 13 && dataColumna < 21) {
+            // console.log(mapa[dataFila][dataColumna]);
+
+            // si no contiene camino o momia a la derecha,arriba,debajo o izquierda 
+            if (!mapa[dataFila - 1][dataColumna].classList.value.includes("camino") &&
+                !mapa[dataFila + 1][dataColumna].classList.value.includes("camino") &&
                 !mapa[dataFila][dataColumna + 1].classList.value.includes("camino") &&
-                !mapa[dataFila + 1][dataColumna].classList.value.includes("camino") 
-                             
-               )  
-                 {     
-                console.log("rodeado");
+                !mapa[dataFila][dataColumna - 1].classList.value.includes("camino")
+
+            ) { //Añadimos divRodeado a cada bloque en caso de que tenga huellas o muros por los lados
+                docBloques[i].classList.add("divRodeado");
             }
-}
+            // comprobamos si los 6 bloques de cada columna estan rodeados 
+            if (docBloques[i].classList.value.includes("divRodeado") &&
+                docBloques[i + 1].classList.value.includes("divRodeado") &&
+                docBloques[i + 2].classList.value.includes("divRodeado") &&
+                docBloques[i + 15].classList.value.includes("divRodeado") &&
+                docBloques[i + 16].classList.value.includes("divRodeado") &&
+                docBloques[i + 17].classList.value.includes("divRodeado")
+            ) { //si estan rodeados cambiamos ese divrodeado por otra clase para que cambie
+                docBloques[i].classList.replace("divRodeado", "llave");
+                docBloques[i + 1].classList.replace("divRodeado", "llave");
+                docBloques[i + 2].classList.replace("divRodeado", "llave");
+                docBloques[i + 15].classList.replace("divRodeado", "llave");
+                docBloques[i + 16].classList.replace("divRodeado", "llave");
+                docBloques[i + 17].classList.replace("divRodeado", "llave");
+
+
+            }
+
+
+
+        }
+    }
 }
