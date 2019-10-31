364b1da48265bfeb39b7d6a282eb09ce61122f01
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-10-31 19:50:33 +0100

    Darle duro Tonight

--- UD02/Tarea_09/drumkit.js
+++ UD02/Tarea_09/drumkit.js
@@ -3,3 +3,17 @@ function ejecutarSonido(e) {
     //REVISAR COMILLAS EJECUCION
 let sonido = document.querySelector(`sonido[data-key="${e.keyCode}"]`);
 }
+
+
+function eliminarTransicion(e) {
+    e.target.classList.remove("");
+}
+
+const tecla= document.querySelector('.keys');
+tecla.forEach(i => { i.addEventListener('transitionend', eliminarTransicion);
+    
+});
+
+
+
+
