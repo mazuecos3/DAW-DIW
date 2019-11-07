d17a08d0660e512cfd1bda2e9e7aef3b4abb4e2a
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-11-07 19:43:03 +0100

    +Acabado Uee

--- UD02/Tarea_12/Tarea_12/script.js
+++ UD02/Tarea_12/Tarea_12/script.js
@@ -11,22 +11,38 @@ function init() {
 
 function movimiento() { document.querySelector("button").addEventListener("click", createDiv); }
 
+//creamos elementos tipo box y de un tamaño de no más de 20 
+// y le añadimos la clase box y se le añade el evento click que hace evolucionar.
 function createDiv() {
 
     let newDiv = document.createElement("box");
+    let contadorDiv = document.querySelectorAll("box").length;
     newDiv.classList.add("box");
-    document.querySelector("container").appendChild(newDiv);
-    newDiv.addEventListener("click", evolucion1);
-
+   
+    //MAXIMO DIVS TIPO BOX
+    if (contadorDiv< 20) {
+        document.querySelector("container").appendChild(newDiv);
+        newDiv.addEventListener("click", evolucion1);
+    }
+    
 }
-
+//Añade la clase evolucionada y también añade evento clickar para que quite la evolución.
 function evolucion1() {
 
     this.classList.add("evoluciona");
-    this.addEventListener("click", evolucion2);
+    this.addEventListener("click", desvoluciona);
+    console.log("Evolucion1");
 }
+//Añade la clase desevolución(Quita efectos) y el evento click que llama a la evolucion2 
+function desvoluciona() {
 
+    this.classList.replace("evoluciona","desevoluciona");
+    this.addEventListener("click", evolucion2);
+    console.log("desevolucion");
+}
+//Convierte a chulu ultimate!!!!!!!!! 
 function evolucion2() {
-
-    this.classList.add("ultimate");
+    this.classList.replace("desevoluciona","ultimate");
+    console.log("Ultimate");
+   
 }
