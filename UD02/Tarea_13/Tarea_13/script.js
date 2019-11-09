b7e79483c5395e88e6b512ede6b812ca95a0241e
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-11-09 13:49:20 +0100

    +Acabado -Preguntar duda (Clickar dos veces)

--- UD02/Tarea_13/Tarea_13/script.js
+++ UD02/Tarea_13/Tarea_13/script.js
@@ -9,12 +9,6 @@ function init() {
     eventoRotar();
 }
 
-/* 
- 
-    ^(;,;)^ : Fragmento perdido
-
-*/
-
 function movimiento() { document.querySelector("button").addEventListener("click", createDiv); }
 
 //creamos elementos tipo box y de un tamaño de no más de 20 
@@ -36,38 +30,68 @@ function evolucion1() {
 
     this.classList.add("evoluciona");
     this.addEventListener("click", desvoluciona);
-    console.log("Evolucion1");
+    //console.log("Evolucion1");
 }
 //Añade la clase desevolución(Quita efectos) y el evento click que llama a la evolucion2 
 function desvoluciona() {
 
     this.classList.replace("evoluciona", "desevoluciona");
     this.addEventListener("click", evolucion2);
-    console.log("desevolucion");
+    //console.log("desevolucion");
 }
 //Convierte a chulu ultimate!!!!!!!!! 
 function evolucion2() {
-    this.classList.replace("desevoluciona", "ultimate");
-    console.log("Ultimate");
+    if (this.classList.value.includes("desevoluciona")) {
+        this.classList.replace("desevoluciona", "ultimate");
+    }
+
+    //console.log("Ultimate");
+    if (this.classList.value.includes("ultimate") && movimientoRotar) {
+        this.addEventListener("click", rotar);
+    }
+    if (this.classList.value.includes("ultimate") && movimientoRebotar) {
+        this.addEventListener("click", rebotar);
+    }
 
 }
-
 
 //Añadir evento click y funcion rotar 
 function eventoRotar() {
-
-    document.getElementById("uno").addEventListener("click", rotar);
-    console.log(movimientoRotar);
+    document.getElementById("uno").addEventListener("click", comprobarRotar);
 }
 
+function comprobarRotar() {
+    //console.log(movimientoRotar);
+    movimientoRotar = true;
+    //console.log(movimientoRotar);
+}
+
+//Añadimos la clase rebotar y volvemos a poner la comprobación a false para que 
+//no ocurra mas veces y tengas que clickar otra vez para cada evento 
 function rotar() {
-    console.log("prueba");
+    if (movimientoRotar && !this.classList.value.includes("rebotar")) {
+        this.classList.add("rotar");
+        movimientoRotar = false;
+    }
 }
 
+//Añadir evento click y funcion rebotar 
 function eventoRebotar() {
-    document.getElementById("dos").addEventListener("click", rebotar);
+    document.getElementById("dos").addEventListener("click", comprobarRebotar);
 }
 
+//Igualar a true para hacer la comprobación
+function comprobarRebotar() {
+    movimientoRebotar = true;
+    //console.log(movimientoRotar);
+}
+
+//Añadimos la clase rebotar y volvemos a poner la comprobación a false para que 
+//no ocurra mas veces y tengas que clickar otra vez para cada evento 
 function rebotar() {
-    console.log("prueba");
+    if (movimientoRebotar && !this.classList.value.includes("rotar")) {
+        this.classList.add("rebotar");
+        movimientoRebotar = false;
+    }
+
 }
