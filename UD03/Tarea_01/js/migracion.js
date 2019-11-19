2c5a29968026148d47784dd01cc0b18ec952848b
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-11-19 02:59:23 +0100

    +Estructura, algo js y algo css -En clase dudas y avanzar js

--- UD03/Tarea_01/js/migracion.js
+++ UD03/Tarea_01/js/migracion.js
@@ -2,8 +2,7 @@
 
 This Script is licensed under GPL v3 or higher
 
-Author: Angel Berlanas Vicente
-email : <berlanas_ang@gva.es>
+Author: Oscar Maxzuecos
 
 */
 
@@ -14,18 +13,39 @@ FUNCIONES PERDIDAS
 
 */
 
-function startMigration(){
+function startMigration() {
 
-    // Fragmentos perdidos
-    // ^(;,;)^
+    //creamos contenedor de todos lo que contiene steps
+    let steps = document.querySelectorAll('steps');
+    //Nos quedamos con los hijos de el contenedor steps
+    steps = document.querySelector('steps').children;
+    let label;
+    let barraProgreso;
+    let mensajeFinal;
+
+    // lo dividimos de tres en tres para poder hacelro de forma dinámica siempre
+    // el primero para label, segundo barraProgreso y el ultimo barra final, así sucesivamente con todos los que hayan.
+    for (let i = 0; i < steps.length; i += 3) {
+
+
+        label = steps[i];
+        barraProgreso = steps[i + 1];
+        mensajeFinal = steps[i + 2];
+        console.log(label);
+        console.log(barraProgreso);
+        console.log(mensajeFinal);
+
+
+    }
+
 }
 
-function init(){
+function init() {
     console.info(" * Init envirnoment ");
 
     // Set click function on button
-    document.querySelector("button").addEventListener("click",startMigration);
+    document.querySelector("button").addEventListener("click", startMigration);
 }
 
 // Init the environment when all is ready
-window.onload=init;
+window.onload = init;
