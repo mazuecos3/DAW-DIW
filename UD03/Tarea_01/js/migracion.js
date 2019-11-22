0196d2b2d983cd73dcee73bc8d1dc40711640a37
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-11-22 18:51:28 +0100

    Hacer en casa

--- UD03/Tarea_01/js/migracion.js
+++ UD03/Tarea_01/js/migracion.js
@@ -6,15 +6,9 @@ Author: Oscar Maxzuecos
 
 */
 
-/*
-
-FUNCIONES PERDIDAS
-^(;,;)^
-
-*/
 
 function startMigration() {
-
+    ocultar();
     //creamos contenedor de todos lo que contiene steps
     let steps = document.querySelectorAll('steps');
     //Nos quedamos con los hijos de el contenedor steps
@@ -23,28 +17,44 @@ function startMigration() {
     let barraProgreso;
     let mensajeFinal;
 
-    // lo dividimos de tres en tres para poder hacelro de forma dinámica siempre
+    // lo dividimos de tres en tres para poder hacerlo de forma dinámica siempre
     // el primero para label, segundo barraProgreso y el ultimo barra final, así sucesivamente con todos los que hayan.
     for (let i = 0; i < steps.length; i += 3) {
 
-
-        label = steps[i];
+        
+        label = steps[i];//.classList.add("steplabel1")
         barraProgreso = steps[i + 1];
         mensajeFinal = steps[i + 2];
-        console.log(label);
+       /* console.log(label);
         console.log(barraProgreso);
         console.log(mensajeFinal);
-
+        */
 
     }
+    
 
+}
+
+function ocultar() {
+    let steps;
+    steps = document.querySelector('steps').children;     
+    for (let i = 0; i < steps.length; i++) {
+      
+        //console.log(steps[i]);
+        steps[i].style.visibility = "visible";
+    }
+}
+
+function efectoLabel() {
+    
 }
 
 function init() {
     console.info(" * Init envirnoment ");
 
     // Set click function on button
-    document.querySelector("button").addEventListener("click", startMigration);
+    document.querySelector("button").addEventListener("click",  startMigration);
+   
 }
 
 // Init the environment when all is ready
