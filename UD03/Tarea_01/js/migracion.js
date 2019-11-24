efb1a58f8ccd71863631e9f3232434f21fc03cd5
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-11-24 16:01:07 +0100

    Añadiendo transiciones

--- UD03/Tarea_01/js/migracion.js
+++ UD03/Tarea_01/js/migracion.js
@@ -6,56 +6,49 @@ Author: Oscar Maxzuecos
 
 */
 
+let steps;
 
 function startMigration() {
-    ocultar();
+
+    document.querySelector("button").removeEventListener("click", startMigration);
     //creamos contenedor de todos lo que contiene steps
-    let steps = document.querySelectorAll('steps');
     //Nos quedamos con los hijos de el contenedor steps
     steps = document.querySelector('steps').children;
-    let label;
-    let barraProgreso;
-    let mensajeFinal;
 
-    // lo dividimos de tres en tres para poder hacerlo de forma dinámica siempre
-    // el primero para label, segundo barraProgreso y el ultimo barra final, así sucesivamente con todos los que hayan.
-    for (let i = 0; i < steps.length; i += 3) {
-
-        
-        label = steps[i];//.classList.add("steplabel1")
-        barraProgreso = steps[i + 1];
-        mensajeFinal = steps[i + 2];
-       /* console.log(label);
-        console.log(barraProgreso);
-        console.log(mensajeFinal);
-        */
-
-    }
-    
-
-}
-
-function ocultar() {
-    let steps;
-    steps = document.querySelector('steps').children;     
     for (let i = 0; i < steps.length; i++) {
-      
-        //console.log(steps[i]);
-        steps[i].style.visibility = "visible";
+
+        steps[i].classList.add("estabaEscondido");
+        steps[i].addEventListener("transitionend", finalizar);
+
+        //el tagname Funciona con Mayusculas, coge el nombre de la etiqueta
+        if (steps[i].tagName == "STEPLABEL") {
+            //console.log(steps[i]);
+            steps[i].classList.add("stepLabel");
+        }
+        if (steps[i].tagName == "PROGRESS") {
+            console.log(steps[i]);
+            steps[i].classList.add("progressBuena");
+        }
+        if (steps[i].tagName == "FINALMSG") {
+            // console.log(steps[i]);
+            steps[i].classList.add("finalmsg");
+        }
     }
 }
 
-function efectoLabel() {
-    
+function finalizar() {
+
+    console.log("perro");
+
 }
+
 
 function init() {
     console.info(" * Init envirnoment ");
-
     // Set click function on button
-    document.querySelector("button").addEventListener("click",  startMigration);
-   
-}
 
+
+    document.querySelector("button").addEventListener("click", startMigration);
+}
 // Init the environment when all is ready
 window.onload = init;
