b015bb4993241e1ffbf243b54637a2bbe24ca72f
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-11-24 18:31:20 +0100

    Casi terminado

--- UD03/Tarea_01/js/migracion.js
+++ UD03/Tarea_01/js/migracion.js
@@ -8,6 +8,9 @@ Author: Oscar Maxzuecos
 let contadorLabels = 0;
 let steps;
 
+//funcion comenzarMigración que asignara quitara el evento click 
+//para que no puedas darle otra vez y evitarte bugs y luego nos guardaremos todos los hijos
+// de steps en una variable y llamamos a la funcion primerLabel
 function startMigration() {
 
     document.querySelector("button").removeEventListener("click", startMigration);
@@ -16,7 +19,9 @@ function startMigration() {
     steps = document.querySelector('steps').children;
     primeraLabel();
 }
-
+//mientras sea menor de el tamaño de los hijos (18)
+//añadimos la clase estabaEscondidio para hacerlo visible y cuando acabe 
+//la transicion pasamos a la siguiente funcion y aumentamos contador para que pase al siguiente item hijo.
 function primeraLabel() {
     if (contadorLabels < steps.length) {
         steps[contadorLabels].classList.add("estabaEscondido");
@@ -24,28 +29,31 @@ function primeraLabel() {
         contadorLabels++;
     }
 }
-
+//añadimos la clase estabaEscondidio para hacerlo visible y la clase para la barra(donde haremos la trnasicion)
+// y cuando acabe la transicion pasamos a la siguiente funcion y
+// aumentamos contador para que pase al siguiente item hijo.
 function progressBarra() {
     steps[contadorLabels].classList.add("estabaEscondido");
-    steps[contadorLabels].classList.add("progerssBuena");
+    steps[contadorLabels].classList.add("progressBuena");
     steps[contadorLabels].addEventListener("transitionend", mensajeFinal);
-    console.log("perro");
+
     contadorLabels++;
-
 }
-
+//añadimos la calse para que sea visible y luego la clase neon también 
+//para añadir la transicion que tiene que hacer el mensaje final
+//no ponemos transitionend en este porque peta bastante preguntar duda en clase
 function mensajeFinal() {
     steps[contadorLabels].classList.add("estabaEscondido");
-    steps[contadorLabels].classList.add("finalmsg");
-    steps[contadorLabels].addEventListener("transitionend", primeraLabel);
+    steps[contadorLabels].classList.add("neon");
     contadorLabels++;
+    primeraLabel();
+
+
 }
 
 function init() {
     console.info(" * Init envirnoment ");
     // Set click function on button
-
-
     document.querySelector("button").addEventListener("click", startMigration);
 }
 // Init the environment when all is ready
