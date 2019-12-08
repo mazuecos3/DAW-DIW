c6c6389ef5011137bb42a18d3fbc86ea667a8f1a
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-12-08 17:15:33 +0100

    Probando y acabando

---
+++ UD04/Unidad04_FormulariosInteractividad/public/js/main.js
@@ -0,0 +1,73 @@
+// Simple script to use with datosAbiertos
+
+// Author : Angel Berlanas Vicente
+//         <berlanas_ang@gva.es>
+// This script is licensed under GPLv3 or higher
+
+
+// Algunos valores
+
+const fuentesUrl = " http://mapas.valencia.es/lanzadera/opendata/kioscos_once/JSON";
+
+// Esta es la funcion de filtrado para
+// obtener tan solo los elementos que cumplen
+// una serie de requisitos.
+
+function filtroLetra(elemento) {
+    let letra = document.querySelector(`input[name="emplazamiento"]`).value;
+    return elemento.properties.calle.startsWith(letra);
+
+}
+
+
+// Pasa a mayuscula el texto de propio input
+// se lanza cada vez que se realiza una insercion en
+// el texto del nombre.
+
+
+
+
+function buscar() {
+
+    // Obtenemos el JSON que esta definido
+    const fetchPromesa = fetch(fuentesUrl);
+
+    // Cuando se resuelva la promesa
+    fetchPromesa.then(response => {
+        // la pasamos a JSON
+        return response.json();
+        // Y entonces
+    }).then(respuesta => {
+        // Filtramos los resultados con el filtro definido anteriormente
+        const resultado = respuesta.features.filter(filtroLetra);
+
+        // Una vez tenemos el listado filtrado pasamos a crear
+        // cada uno de los <li> que representan
+        let listado = document.createElement("ul");
+
+        // Por cada uno de ellos 
+        resultado.forEach(fuente => {
+            // Creamos un <li>
+            let calleli = document.createElement("li");
+            calleli.innerHTML = fuente.properties.calle + " -- [" + fuente.geometry.coordinates + "]";
+            // Lo anyadimos
+            listado.appendChild(calleli);
+        });
+        // Establecemos el listado en la Web
+        document.querySelector(".resultados").innerHTML = "";
+        document.querySelector(".resultados").appendChild(listado);
+    });
+
+}
+
+function init() {
+
+    // Binding de los eventos correspondientes.
+
+    // Click en el boton de buscar
+    document.querySelector(`input[type="button"]`).addEventListener("click", buscar);
+
+}
+
+// The mother of the lamb.
+window.onload = init;
