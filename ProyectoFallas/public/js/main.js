1d966f972d156711ed52f616d1063b084f42e33c
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-12-10 20:25:23 +0100

    +Mostrando fotos fallas -Grid

--- ProyectoFallas/public/js/main.js
+++ ProyectoFallas/public/js/main.js
@@ -1,14 +1,14 @@
 // Algunos valores
-const fuentesUrl = " http://mapas.valencia.es/lanzadera/opendata/kioscos_once/JSON";
+const fuentesUrl = "http://mapas.valencia.es/lanzadera/opendata/Monumentos_falleros/JSON";
 
 // Esta es la funcion de filtrado para
 // obtener tan solo los elementos que cumplen
 // una serie de requisitos.
 
 function filtroLetra(elemento) {
-    let letra = document.querySelector(`input[name="emplazamiento"]`).value;
+    let letra = document.querySelector(`input[name="falla"]`).value;
     //starsWith (con la letra que empieza)
-    return elemento.properties.emplazamiento.startsWith(letra);
+    return elemento.properties.nombre.startsWith(letra);
 }
 
 function buscar() {
@@ -27,19 +27,18 @@ function buscar() {
 
         // Una vez tenemos el listado filtrado pasamos a crear
         // cada uno de los <li> que representan
-        let listado = document.createElement("ul");
+       let divResultados = document.getElementById("resultados");
 
         // Por cada uno de ellos 
-        resultado.forEach(kiosco => {
+        resultado.forEach(fallas => {
             // Creamos un <li>
-            let calleli = document.createElement("li");
-            calleli.innerHTML = kiosco.properties.emplazamiento + " -- [" + kiosco.geometry.coordinates + "]";
+            let calleli = document.createElement("div");
+            calleli.innerHTML = "<img src=" + fallas.properties.boceto + ">"  + fallas.properties.nombre ;
             // Lo anyadimos
-            listado.appendChild(calleli);
+            divResultados.appendChild(calleli);
         });
-        // Establecemos el listado en la Web
-        document.querySelector(".resultados").innerHTML = "";
-        document.querySelector(".resultados").appendChild(listado);
+       
+      
     });
 
 }
