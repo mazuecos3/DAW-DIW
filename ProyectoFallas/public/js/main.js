050b30605360c089d3cf11cc0d3be9388ff2535a
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-12-12 02:06:50 +0100

    Imagenes / Texto / Botton terminado -Preguntar Dudas filter Section

--- ProyectoFallas/public/js/main.js
+++ ProyectoFallas/public/js/main.js
@@ -1,60 +1,80 @@
 // Direccion json web
 const fuentesUrl = "http://mapas.valencia.es/lanzadera/opendata/Monumentos_falleros/JSON";
 
-
 function buscar() {
-
-    // Obtenemos el JSON que esta definido
+    // Obtenemos el JSON que esta definido.
     const fetchPromesa = fetch(fuentesUrl);
 
-    // Cuando se resuelva la promesa
+    // Cuando se resuelva la promesa.
     fetchPromesa.then(response => {
-        // la pasamos a JSON
+        // la pasamos a JSON.
         return response.json();
-        // Y entonces
+        // Y entonces.
     }).then(respuesta => {
-        // resultados
+        // resultados.
         const resultado = respuesta.features;
 
         let divResultados = document.getElementById("resultados");
         let divTodasFallas = document.createElement("div");
         divTodasFallas.classList.add("fallas");
+
         // Por cada uno de ellos 
         resultado.forEach(fallas => {
-            let fallaPrincipal = document.getElementById("fallaPrincipal");
-            let fallaInfantil = document.getElementById("fallaInfantil");     
+            //DUDAS CON EL FILTER PREGUNTAR A ANGEL
+            establecerSection(fallas);
+
+            //Creamos un div para cada falla y le añadimos la clase.
             let divFalla = document.createElement("div");
-
             divFalla.classList.add("falla");
-            if ( document.getElementById("fallaPrincipal").checked == true) {
-                divFalla.innerHTML = "<img src=" + fallas.properties.boceto + "><br>" + fallas.properties.nombre + "<br><button>Ubicación</button>"; 
-            }
-            if ( document.getElementById("fallaInfantil").checked == true) {
-                divFalla.innerHTML = "<img src=" + fallas.properties.boceto_i + "><br>" + fallas.properties.nombre + "<br><button>Ubicación</button>"; 
-              }
 
+            //Si esta pulsado el radiobutton de la falla pirncipal establecemos las fotos de las fallas principales.
+            if (document.getElementById("fallaPrincipal").checked == true) {
+                divFalla.innerHTML = "<img src=" + fallas.properties.boceto + "><br>" + fallas.properties.nombre + "<br><button>Ubicación</button>";
+            } //Si esta pulsado el radiobutton de la falla infantil establecemos las fotos de las fallas infantil.
+            if (document.getElementById("fallaInfantil").checked == true) {
+                divFalla.innerHTML = "<img src=" + fallas.properties.boceto_i + "><br>" + fallas.properties.nombre + "<br><button>Ubicación</button>";
+            }
             //añadimos la falla 
             divTodasFallas.appendChild(divFalla);
         });
-
+        // Lo establecemos en blanco cada vez que se haga la funcion para hacer efecto refrescar.
         divResultados.innerHTML = "";
+        //Añadimos las falla al div principal de los resultados.
         divResultados.appendChild(divTodasFallas);
-
-
     });
 
 }
-//funcion inicial
+
+
+
+
+function establecerSection(fallas) {
+
+    let allSection = document.getElementById("section");
+    let option = document.createElement("option");
+    option.innerHTML = fallas.properties.seccion;
+
+    allSection.appendChild(option);
+
+    //DUDAS CON EL FILTER
+    let selectValue = allSection[allSection.selectedIndex].value;
+    console.log(fallas.properties.seccion.startsWith(selectValue))
+    return fallas.properties.seccion.startsWith(selectValue);
+
+
+}
+//Funcion inicial
 function init() {
-   
-    //los dos radiobutons
+    //llamamos a la funcion buscar nada mas empezar porque el radiobuton principal va a estar pulsado por defecto
+    //para que al entrar muestre todas las fallas principales directamente.
+    buscar();
+    //Añadimos evento click a cada radiobutton.
     document.querySelectorAll(`input[name="fallaPri"]`)[0].addEventListener("click", buscar);
     document.querySelectorAll(`input[name="fallaPri"]`)[1].addEventListener("click", buscar);
-    
-  
-    // Click en el boton de buscar
-  
+    //Funcion changue para el section
+    document.getElementById("section").addEventListener("change", buscar);
 }
 
-//funcion inicial al cargar la pagina
+
+//Funcion inicial al cargar la pagina.
 window.onload = init;
