fe42e908824255fa6302b757e76a3124b14f9eea
mazuecos3 <oscar3mazuecos@hotmail.com>
2019-12-16 01:48:42 +0100

    Mejorando Estructura

--- ProyectoFallas/public/js/main.js
+++ ProyectoFallas/public/js/main.js
@@ -21,6 +21,7 @@ function buscar() {
     });
 
 }
+
 function rellenarSection() {
     // Recogemos las secciones  las metemos en un array.
     for (let i = 0; i < resultadoJSON.length; i++) {
@@ -45,25 +46,11 @@ function rellenarSection() {
     //console.log(secciones);
 }
 
-function fechas() {
-    let fecha1 = document.getElementById("fecha1");
-    let fecha2 = document.getElementById("fecha2");
-   // console.log(fecha1.value);
-   // console.log(fecha2.value);
 
-    for (let i = 0; i < resultadoJSON.length; i++) {
-        let fechaPrincipal = resultadoJSON[i].properties.anyo_fundacion;
-        let fechaInfantil = resultadoJSON[i].properties.anyo_fundacion_i;
-       
-        console.log(fechaPrincipal);
-    }
-   
-}
 
 function principal() {
 
-    let principalChecked = document.getElementById("fallaPrincipal").checked; 
-    let infantilChecked = document.getElementById("fallaInfantil").checked; 
+    let principalChecked = document.getElementById("fallaPrincipal").checked;
     let divResultados = document.getElementById("resultados");
     let divTodasFallas = document.createElement("div");
     divTodasFallas.classList.add("fallas");
@@ -77,47 +64,77 @@ function principal() {
         let secciones = document.getElementById("section");
         let fecha1 = document.getElementById("fecha1");
         let fecha2 = document.getElementById("fecha2");
-       
-        //Primer valor de ejemplo que muestra todas las fallas
+        // RANGO FECHAS
+        let boceto;
 
-        if (fecha1.value < fecha2.value && fecha1.value == fallas.properties.anyo_fundacion && fecha1.value == fallas.properties.anyo_fundacion) {
+        // if (fecha1.value < fecha2.value && fecha1.value < fallas.properties.anyo_fundacion && fecha2.value > fallas.properties.anyo_fundacion) {
 
         if (secciones.value == "Todas") {
-            
             //Si esta pulsado el radiobutton de la falla pirncipal establecemos las fotos de las fallas principales.
             if (principalChecked) {
-                divFalla.innerHTML = "<img src=" + fallas.properties.boceto + ">" + fallas.properties.nombre + "<button>Ubicación</button>";
+                boceto = fallas.properties.boceto;
             } //Si esta pulsado el radiobutton de la falla infantil establecemos las fotos de las fallas infantil.
-            if (infantilChecked ) {
-                divFalla.innerHTML = "<img src=" + fallas.properties.boceto_i + ">" + fallas.properties.nombre + "<button>Ubicación</button>";
+            else {
+                boceto = fallas.properties.boceto_i;
             }
-            //añadimos la falla 
-            divTodasFallas.appendChild(divFalla);
             //depende de el valor de la seccion se mostrará aquellos que entren en esa seccion.
         } else if (fallas.properties.seccion == secciones.value) {
+
             //Si esta pulsado el radiobutton de la falla pirncipal establecemos las fotos de las fallas principales.
             if (principalChecked) {
-                divFalla.innerHTML = "<img src=" + fallas.properties.boceto + ">" + fallas.properties.nombre + "<button>Ubicación</button>";
-            } //Si esta pulsado el radiobutton de la falla infantil establecemos las fotos de las fallas infantil.
-            if (infantilChecked ) {
-                divFalla.innerHTML = "<img src=" + fallas.properties.boceto_i + ">" + fallas.properties.nombre + "<button>Ubicación</button>";
+                console.log("antes " + boceto);
+                boceto = fallas.properties.boceto;
+                console.log("despues " + boceto);
+            }
+            //Si esta pulsado el radiobutton de la falla infantil establecemos las fotos de las fallas infantil.
+            else {
+                boceto = fallas.properties.boceto_i;
+                console.log("antes " + boceto);
             }
             //añadimos la falla 
-            divTodasFallas.appendChild(divFalla);
         }
-    }
+        //   }
+
+        let imagen = document.createElement("img");
+        imagen.src = boceto;
+
+        let nombre = document.createElement("p");
+        nombre.innerText = fallas.properties.nombre;
+
+        let botonUbi = document.createElement("button");
+        botonUbi.innerText = "Ubicación";
+
+        let botonInfo = document.createElement("button");
+        botonInfo.innerText = "Más Información";
+
+        divFalla.appendChild(imagen);
+        divFalla.appendChild(nombre);
+        divFalla.appendChild(botonUbi);
+        divFalla.appendChild(botonInfo);
+        divTodasFallas.appendChild(divFalla);
+
     });
+
     // Lo establecemos en blanco cada vez que se haga la funcion para hacer efecto refrescar.
     divResultados.innerHTML = "";
     //Añadimos las falla al div principal de los resultados.
     divResultados.appendChild(divTodasFallas);
 
-    fechas();
-   
+
 }
 
+function limpiar() {
+    let divResultados = document.getElementById("resultados");
+    divResultados.innerHTML = "";
+}
 
+function mostrarMapa() {
+    console.log("perro1")
+}
 
+function mostrarInfo() {
+    console.log("perro2")
+}
 //Funcion inicial
 function init() {
     //llamamos a la funcion buscar nada mas empezar porque el radiobuton principal va a estar pulsado por defecto
@@ -127,11 +144,15 @@ function init() {
     document.querySelectorAll(`input[name="fallaPri"]`)[0].addEventListener("click", principal);
     document.querySelectorAll(`input[name="fallaPri"]`)[1].addEventListener("click", principal);
     //Funcion changue para el section
-   document.getElementById("section").addEventListener("change", principal);
-   document.getElementById("fecha1").addEventListener("blur",principal);
-   document.getElementById("fecha2").addEventListener("blur",principal);
+    document.getElementById("section").addEventListener("change", principal);
+    //Funcion perdida de foco
+    document.getElementById("fecha1").addEventListener("blur", principal);
+    document.getElementById("fecha2").addEventListener("blur", principal);
+
+
+
+
+
 }
-
-
 //Funcion inicial al cargar la pagina.
 window.onload = init;
