1c85db06824b123b63206f8b35704e337b8c0e77
mazuecos3 <oscar3mazuecos@hotmail.com>
2020-01-17 18:15:22 +0100

    Estructuring mas

--- UD04/Canvas/Tarea01-Graficos/graficos.js
+++ UD04/Canvas/Tarea01-Graficos/graficos.js
@@ -1,5 +1,38 @@
 function buildGrafico(){
     console.info(" * Construyendo grafico ");
+    const canvas = document.querySelector("canvas");
+    // contexto -> ctx
+    let ctx = canvas.getContext("2d");
+
+    ctx.fillStyle="red";
+
+    //ctx.fillRect(10,10,100,200);
+    ctx.strokeStyle="purple";
+    ctx.lineWidth=1;
+    //ctx.strokeRect(10,10,100,200);
+
+    ctx.beginPath();
+    ctx.moveTo(100,100);
+    ctx.lineTo(50,50);
+    ctx.lineTo(50,100);
+    ctx.lineTo(100,100);
+    //ctx.fill();
+    ctx.stroke();
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    ctx.moveTo(200,200);
+
+    // control, destino
+    //ctx.quadraticCurveTo(100,100,200,0);
+    //ctx.bezierCurveTo(100,100,70,70,200,0);
+
+    ctx.beginPath();
+
+    //ctx.arc(200,200,50,0.2,1 * Math.PI);
+
+    ctx.stroke();
+    console.log(ctx);
 }
 
 function loadListeners(){
@@ -8,8 +41,10 @@ function loadListeners(){
 
 
 function init(){
-    console.log(" * Init ");
+    //console.log(" * Init ");
     loadListeners();
+  
+
     
 }
 
