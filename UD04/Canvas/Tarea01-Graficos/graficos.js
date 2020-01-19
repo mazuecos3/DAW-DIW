5939596bd14c7b479ad650afeb9d5a8c3ebb7609
mazuecos3 <oscar3mazuecos@hotmail.com>
2020-01-19 12:42:38 +0100

    Conseguido grafico barras faltan lineal y tarta

--- UD04/Canvas/Tarea01-Graficos/graficos.js
+++ UD04/Canvas/Tarea01-Graficos/graficos.js
@@ -1,39 +1,52 @@
-let dioses = [];
-let tipoGrafico;
-let color = ["green", "red", "orange", "purple"]
-
 //datos añadidos como json  (array clave - valor)
 function main() {
 
     let nombre = document.querySelectorAll("input[class='left']");
     let poder = document.querySelectorAll("input[class='right']");
-
+    let dioses = [];
+    let color = ["green", "red", "orange", "purple"]
     for (let i = 0; i < nombre.length; i++) {
 
         dioses.push({
             "nombre": nombre[i].value,
-            "poder": poder[i].value,
+            "poder": parseInt(poder[i].value),
             "color": color[i]
         });
 
+
     }
 
-    console.log(dioses);
+    graficoRayas(dioses);
     tarta(dioses);
-    rectangulos(dioses);
     lineas(dioses);
+
 
 }
 
 //grafico rectangular
-function rectangulos() {
-    console.log(" * Construyendo grafico rectangular")
+function graficoRayas(dioses) {
+    console.log("Creando grafico rayas")
     const canvas = document.getElementById("canvas1");
-
     let ctx = canvas.getContext("2d");
 
+    let width = canvas.width;
+    let height = canvas.height;
+
+    const poderTotal = sumaPoderes(dioses);
+    const barra = canvas.width / dioses.length;
+
+
+    for (let i = 0; i < dioses.length; i++) {
+        const dios = dioses[i];
+        ctx.fillStyle = dios.color;
+        let altura = Math.round(width * dios.poder / (poderTotal + 20))
+        ctx.fillRect(barra * i, height - altura, barra, altura);
+
+    }
 
 }
+
+
 
 //grafico lineal
 function lineas() {
@@ -49,6 +62,17 @@ function tarta() {
     console.info(" * Construyendo grafico tarta ");
     const canvas = document.getElementById("canvas3");
     let ctx = canvas.getContext("2d");
+}
+
+function sumaPoderes(dioses) {
+    let poderio = 0;
+
+    for (let i = 0; i < dioses.length; i++) {
+        const dios = dioses[i];
+        poderio += dios.poder;
+    }
+    console.log(poderio);
+    return poderio;
 }
 
 function init() {
